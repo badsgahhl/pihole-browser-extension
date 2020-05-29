9ad1fc658d2f296b92ea8982127ff761ffea031c
Pascal Glaser <pascal@glaser.casa>
2020-05-29 11:38:11 +0200

    Fixes for build and permission fixes

--- create_package.sh
+++ create_package.sh
@@ -1,4 +1,7 @@
-rm package.zip
+rm package.chrome.zip
+rm package.firefox.zip
 
-zip -r package.zip dist/ icon/ src/ manifest.json -x "*.ts"
-
+zip -r package.chrome.zip dist/ icon/ src/ manifest.chrome.json -x "*.ts"
+printf "@ manifest.chrome.json\n@=manifest.json\n" | zipnote -w package.chrome.zip
+zip -r package.firefox.zip dist/ icon/ src/ manifest.firefox.json -x "*.ts"
+printf "@ manifest.firefox.json\n@=manifest.json\n" | zipnote -w package.firefox.zip
