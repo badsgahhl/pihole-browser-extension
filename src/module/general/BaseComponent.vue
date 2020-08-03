a4cbdea82a05726d7e33f3c0a6cf7f8e32c4830a
Pascal Glaser <pascal@glaser.casa>
2020-08-03 20:36:57 +0200

    Version increase + Bugfixes + Updates

--- src/module/general/BaseComponent.vue
+++ src/module/general/BaseComponent.vue
@@ -1,33 +1,33 @@
 <script lang="ts">
-	import Vue from "vue";
-  import {Component, Prop} from "vue-property-decorator";
-	import {i18nOptionsKeys, i18nPopupKeys, i18nService, LinkConfig} from "../../service/browser/i18nService";
-	import {PiHoleSettingsDefaults} from "../../service/browser/StorageService";
+import Vue from "vue";
+import {Component} from "vue-property-decorator";
+import {i18nOptionsKeys, i18nPopupKeys, i18nService, LinkConfig} from "../../service/browser/i18nService";
+import {PiHoleSettingsDefaults} from "../../service/browser/StorageService";
 
-	@Component
-	export default abstract class BaseComponent extends Vue
-	{
+@Component
+export default class BaseComponent extends Vue
+{
+  // Translation Keys for the Options. Needed for the vue template
+  protected i18nOptionsKeys = i18nOptionsKeys;
 
-		@Prop({default: () => i18nOptionsKeys})
-		i18nOptionsKeys!: typeof i18nOptionsKeys;
+  // Translation Keys for the Popup
+  protected i18nPopupKeys = i18nPopupKeys;
 
-		@Prop({default: () => i18nPopupKeys})
-		i18nPopupKeys!: typeof i18nPopupKeys;
+  // Enum with some Links
+  protected LinkConfig = LinkConfig;
 
-		@Prop({default: () => LinkConfig})
-		LinkConfig!: typeof LinkConfig;
+  // Default Settings
+  protected PiHoleSettingsDefaults = PiHoleSettingsDefaults;
 
-		@Prop({default: () => PiHoleSettingsDefaults})
-		PiHoleSettingsDefaults!: typeof PiHoleSettingsDefaults;
+  /**
+   * Wrapper for translation
+   * @param string
+   * @param substitutions
+   */
+  protected translate(string: string, substitutions?: any): string
+  {
+    return i18nService.translate(string, substitutions);
+  }
 
-		/**
-		 * Wrapper for translation
-		 * @param string
-		 */
-		protected translate(string: string, substitutions?: any): string
-		{
-			return i18nService.translate(string, substitutions);
-		}
-
-	}
+}
 </script>
