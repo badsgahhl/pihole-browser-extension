e1360920ee0d0b9cab42470a4396e5c42efbc1ff
Pascal Glaser <pascal@glaser.casa>
2021-11-14 22:20:48 +0100

    Always dark mode Bug Fixes

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,17 +5,13 @@ import { UserVuetifyPreset } from 'vuetify/types/services/presets'
 
 Vue.use(Vuetify)
 
-const mq = window.matchMedia('(prefers-color-scheme: dark)')
 const opts: Partial<UserVuetifyPreset> = {
   icons: {
     iconfont: 'mdiSvg'
   },
-  theme: { dark: mq.matches }
+  theme: { dark: true }
 }
 
 const vuetify = new Vuetify(opts)
 
-mq.addEventListener('change', e => {
-  vuetify.framework.theme.dark = e.matches
-})
 export default vuetify
